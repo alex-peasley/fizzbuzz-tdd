c595780709634dab4326c1bdb3bde544104323f3
Alex Peasley <alex.peasley95@gmail.com>
2022-02-27 20:37:31 +0000

    Initialise spring boot application

---
+++ src/main/java/com/alexpeasley/fizzbuzztdd/FizzbuzzTddApplication.java
@@ -0,0 +1,13 @@
+package com.alexpeasley.fizzbuzztdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FizzbuzzTddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FizzbuzzTddApplication.class, args);
+	}
+
+}
